fe328ddece4dbc0ecc55f3cff740289e4267358b
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2025-02-13 19:41:39 +0200

    Logic added to extend and close up the sidebar

--- frontend/src/app/components/Sidebar.tsx
+++ frontend/src/app/components/Sidebar.tsx
@@ -1,12 +1,20 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState<boolean>(false);
   return (
-    <div className="flex flex-col h-screen bg-primary text-black w-56">
-      <div className="flex flex-row">
-        <h1 className="m-5 text-3xl justify-center">Tracker</h1>
+    <div
+      className={`flex flex-col h-screen bg-primary text-black ${
+        collapsed ? "w-56" : "w-36"
+      }`}
+    >
+      <div className="flex flex-row justify-around">
+        <h1 className={`m-5 ${collapsed ? "text-3xl" : "text-xl"}`}>Tracker</h1>
         <Image
-          src="./angle-left.svg"
+          src={`${collapsed ? "./angle-left.svg" : "./angle-right.svg"}`}
+          onClick={() => setCollapsed(!collapsed)}
           alt="hide"
           width={24}
           height={24}
@@ -15,57 +23,27 @@ export const Sidebar = () => {
       </div>
 
       <ul className=" flex-1">
-        <li className="flex flex-row m-5">
-          <Image
-            src="./home.svg"
-            alt="logo"
-            width={32}
-            height={32}
-            className="mr-10"
-          />
-          <div className="content-end"> Home </div>
+        <li className="flex flex-row m-5 justify-around">
+          <Image src="./home.svg" alt="logo" width={32} height={32} />
+          {collapsed && <div className="content-end"> Home </div>}
         </li>
-        <li className="flex flex-row m-5">
-          <Image
-            src="./user.svg"
-            alt="logo"
-            width={32}
-            height={32}
-            className="mr-10"
-          />
-          <div className="content-end"> Profile </div>
+        <li className="flex flex-row m-5 justify-around">
+          <Image src="./user.svg" alt="logo" width={32} height={32} />
+          {collapsed && <div className="content-end"> Profile </div>}
         </li>
-        <li className="flex flex-row m-5">
-          <Image
-            src="./running.svg"
-            alt="logo"
-            width={32}
-            height={32}
-            className="mr-10"
-          />
-          <div className="content-end"> Activity </div>
+        <li className="flex flex-row m-5 justify-around">
+          <Image src="./running.svg" alt="logo" width={32} height={32} />
+          {collapsed && <div className="content-end"> Activity </div>}
         </li>
-        <li className="flex flex-row m-5">
-          <Image
-            src="./stats.svg"
-            alt="logo"
-            width={32}
-            height={32}
-            className="mr-10"
-          />
-          <div className="content-end"> Statistics </div>
+        <li className="flex flex-row m-5 justify-around">
+          <Image src="./stats.svg" alt="logo" width={32} height={32} />
+          {collapsed && <div className="content-end"> Statistics </div>}
         </li>
       </ul>
       <div>
-        <li className=" mt-auto flex flex-row m-5 ">
-          <Image
-            src="./exit.svg"
-            alt="logo"
-            width={32}
-            height={32}
-            className="mr-10"
-          />
-          <div className="content-end"> Sign out </div>
+        <li className=" mt-auto flex flex-row m-5 justify-around">
+          <Image src="./exit.svg" alt="logo" width={32} height={32} />
+          {collapsed && <div className="content-end"> Sign out </div>}
         </li>
       </div>
     </div>
